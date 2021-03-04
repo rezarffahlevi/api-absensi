6177bae887cb0a9ed873ee365d904050dc0cd8ee
rezarffahlevi <rezarffahlevi@gmail.com>
2021-03-04 12:39:22 +0700

    base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/api-absensi/';
+	public $baseURL = 'http://api-absensiku.herokuapp.com/';
 
 	/*
 	|--------------------------------------------------------------------------
